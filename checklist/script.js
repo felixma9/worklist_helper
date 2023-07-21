3f82849987cd70b92b1f70c00732c99df57dbd57
NakulGD <nakulgd@gmail.com>
2023-07-21 19:27:36 -0400

    Checklist implementation 2
    Bug fixes:
    - Removed ability to add invisible courses
    - Removed spacing issues
    - Removed alignment issues
    - Removed ability to add invalid time range for courses
    - Fixed time input getting cutoff
    
    Other:
    - Added wave features to page
    - Fixed colour scheme
    - Fixed font
    - Added course-detail input info

--- checklist/script.js
+++ checklist/script.js
@@ -1,56 +1,96 @@
+
+//Responsible for page function after user presses "continue"
 document.getElementById("continue-button").addEventListener("click", function() {
-    document.getElementById("course-details").style.display = "block"
+    
+    //Check to see if the input is empty
+    if (!document.getElementById("input-box").value) {
+        alert("Please enter a course!")
+    } else {
 
-    var rowElement = document.getElementsByClassName("row")[0];
-    rowElement.style.maxHeight = rowElement.scrollHeight + "px";
+        //Reveals the course-details page element
+        document.getElementById("course-details").style.display = "block"
 
-    setTimeout(function() {
-        rowElement.style.maxHeight = "0px";
-    }, 0);
+        //Responsible for the "closing" animation of the initial text input box
+        var rowElement = document.getElementsByClassName("row")[0];
+        rowElement.style.maxHeight = rowElement.scrollHeight + "px";
+        setTimeout(function() {
+            rowElement.style.maxHeight = "0px";
+        }, 0);
 
-    var inputBoxValue = document.getElementById('input-box').value;
-    var courseName = document.createElement('h3');
+        //Adds the user's input text as a heading of the course-details page
+        var inputBoxValue = document.getElementById('input-box').value;
+        var courseName = document.createElement('h3');
+        courseName.innerText = inputBoxValue;
+        courseName.className = "course-name"
+        var courseDetails = document.getElementById('course-details');
+        courseDetails.prepend(courseName);
 
-    courseName.innerText = inputBoxValue;
-    courseName.className = "course-name"
-
-    var courseDetails = document.getElementById('course-details');
-    courseDetails.prepend(courseName);
-    document.getElementById('input-box').value = "";
-});
-
-document.getElementById("choose-button").addEventListener("click", function() {
-    var startTime = document.getElementById('start-time').value;
-    var endTime = document.getElementById('end-time').value;
-    var dayMon = document.getElementById('monBox').value;
-    var dayTue = document.getElementById('tueBox').value;
-    var dayWed = document.getElementById('wedBox').value;
-    var dayThu = document.getElementById('thuBox').value;
-    var dayFri = document.getElementById('friBox').value;
-    var listContainer = document.getElementById("list-container");
-
-    var courseNameElement = document.querySelector(".course-name");
-    if (courseNameElement) {
-        let li = document.createElement("li");
-        li.innerHTML = courseNameElement.innerText;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-        courseNameElement.remove();
+        //Clears the input field of the initial text box
+        document.getElementById('input-box').value = "";
     }
+});
 
-    document.getElementById("course-details").style.display = "none";
+//Responsible for page function after user presses "accept"
+document.getElementById("choose-button").addEventListener("click", function() {
 
-    var rowElement = document.getElementsByClassName("row")[0];
-    rowElement.style.maxHeight = rowElement.scrollHeight + "px";
+    
+    //Checks validity of the user input
+    if (!document.getElementById("start-time").value === "" || document.getElementById("start-time").value >= document.getElementById("end-time").value) {
+        alert("Please enter a valid time range.")
+    } else {
 
-    setTimeout(function() {
-        rowElement.style.maxHeight = "100px";
-    }, 0);
+        //Obtaining the start-time/end-time inputs from the user
+        var startTime = document.getElementById('start-time').value;
+        var endTime = document.getElementById('end-time').value;
+
+        //Obtaining the days-of-the-week input from the user (stores it in an array)
+        var checkBoxes = document.querySelectorAll(".form-check-input");
+        var checkedDays = [];
+        for (var i = 0; i < checkBoxes.length; i++) {
+            if (checkBoxes[i].checked) {
+                checkedDays.push(checkBoxes[i].value)
+            }
+        }
+
+        //Obtaining the checklist element and the course-name  
+        var listContainer = document.getElementById("list-container");
+        var courseNameElement = document.querySelector(".course-name");     //I don't remember what this is actually doing
+
+        //Adds the course to a checklist along with a 'remove cross'
+        if (courseNameElement) {
+            let li = document.createElement("li");
+            li.innerHTML = courseNameElement.innerText;
+            listContainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+            courseNameElement.remove();
+        }
+
+
+        //Hides the course-details box
+        document.getElementById("course-details").style.display = "none";
+
+        //Responsible for animation of initial "row" text field re-appearing
+        var rowElement = document.getElementsByClassName("row")[0];
+        rowElement.style.maxHeight = rowElement.scrollHeight + "px";
+        setTimeout(function() {
+            rowElement.style.maxHeight = "100px";
+        }, 0);
+
+
+        //Creates an object representing the course with all the info provided
+        var course = {
+            name: courseNameElement.innerText,
+            start: startTime,
+            end: endTime,
+            days: checkedDays,
+        }
+    }
 });
 
 
+//Responsible for page function after user clicks checklist item or "remove" on checklist item
 document.getElementById('list-container').addEventListener("click", function(e){
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
@@ -60,4 +100,5 @@ document.getElementById('list-container').addEventListener("click", function(e){
     
 }, false);
 
+//Initially hides the course details page
 document.getElementById("course-details").style.display = "none";
